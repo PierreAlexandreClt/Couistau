9f1d35cfc99306280899f7632773ef2faeb454ad
PierreAlexandreClt <clayetp@yahoo.com>
2015-10-06 11:43:27 +0200

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,30 +1,46 @@
 
-include string
+//
+//  Table.hpp
+
+
+#ifndef Table_hpp
+#define Table_hpp
+
+#include <stdio.h>
+
+
 
 class Table
 {
-private : 
-int : m_idTable;
-Reservation : m_reservation ;
-Commande: m_commande ; 
-bool : m_disponnibilite ; 
-std::string : m_type ; 
-bool : m_localisation ; 
+    private :
+    int : m_idTable;
+    int : m_nbrPlace;
+    bool : m_disponnibilite ;
+    std::string : m_type ;
+    std::string : m_localisation ;
+    
+    
+    public :
+    //Constructeur de table
+    Table (int m_idTable, Commande m_commande, bool m_disponnibilite, std::string m_type, bool m_localisation)
 
-public : 
-//Constructeur de table
-Table (int m_idTable, Commande m_commande, bool m_disponnibilite, std::string m_type, bool m_localisation)
-
-// Fonctions nécessaires pour manipuler les attribus de la table. 
-void setidTable () ; 
-void setReservation (reservation) ;
-void setCommande () ;
-void setType () ;
-
-// Fonctions nécessaires pour voir les attributs de la table. 
-void getTable () ; 
-void getidTable () ;
-void getReservation () ;
-void getType () ;
+    
+    // Fonctions nécessaires pour voir les attributs de la table.
+    void getTable () ; //Fonction qui renvoie l'integralite des informations d'une Table.
+    int getidTable () ;
+    int getNbrPlace () ;
+    void getType () ;
+    void getLocalisation () ;
+    bool getDisponnibilite () ;
+   
+   
+    
+    // Fonctions nécessaires pour manipuler les attribus de la table.
+    void setidTable (int a) ;
+    void setNbrPlace (int a) ;
+    void setType (std::string a) ;
+    void setLocalisation (std::string a);
+    void setDisponnibilite (std::string a) ;
 
 };
+
